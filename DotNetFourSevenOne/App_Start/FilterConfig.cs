dcb25100a5e7c94db8839a4fd749bfd6a0265881
Adam Pritzl <adam.pritzl@centare.com>
2018-10-23 21:03:55 -0500

    Added NET 4.5.2, 4.7.1, Core 2.1

---
+++ DotNetFourSevenOne/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetFourSevenOne
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DotNetFourSevenOne/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DotNetFourSevenOne.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
