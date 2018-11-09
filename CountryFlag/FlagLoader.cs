b50ff352207c6678462b2e478554d05dc17b403e
Adam Pritzl <adam.pritzl@centare.com>
2018-11-09 16:04:21 -0600

    Added country code mappings

--- CountryFlag/FlagLoader.cs
+++ CountryFlag/FlagLoader.cs
@@ -12,6 +12,66 @@ namespace CountryFlag
                 case "USA":
                     countryCode = "us";
                     break;
+                case "Finland":
+                    countryCode = "fl";
+                    break;
+                case "Italy":
+                    countryCode = "it";
+                    break;
+                case "Brazil":
+                    countryCode = "br";
+                    break;
+                case "Germany":
+                    countryCode = "de";
+                    break;
+                case "Switzerland":
+                    countryCode = "ch";
+                    break;
+                case "Mexico":
+                    countryCode = "mx";
+                    break;
+                case "Sweden":
+                    countryCode = "se";
+                    break;
+                case "Argentina":
+                    countryCode = "ar";
+                    break;
+                case "Austria":
+                    countryCode = "at";
+                    break;
+                case "UK":
+                    countryCode = "gb";
+                    break;
+                case "Poland":
+                    countryCode = "pl";
+                    break;
+                case "Canada":
+                    countryCode = "ca";
+                    break;
+                case "Ireland":
+                    countryCode = "ie";
+                    break;
+                case "Norway":
+                    countryCode = "no";
+                    break;
+                case "France":
+                    countryCode = "fr";
+                    break;
+                case "Belgium":
+                    countryCode = "be";
+                    break;
+                case "Spain":
+                    countryCode = "es";
+                    break;
+                case "Venezuela":
+                    countryCode = "ve";
+                    break;
+                case "Denmark":
+                    countryCode = "dk";
+                    break;
+                case "Portugal":
+                    countryCode = "pt";
+                    break;
                 default:
                     throw new Exception($"{countryName} has unsupported flag.");
             }
